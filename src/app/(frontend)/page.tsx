50c3a5d657c616d7dbfd4e1ded0a0b6df339d6ad
Thaniwat <655021000352@mail.rmutk.ac.th.com>
2025-12-24 16:35:56 +0700

    create badge

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import { Button } from '@/components/commons/button'
+import { Badge } from '@/components/commons/badge'
+import { CodeIcon, Database, Cpu, Globe, CheckCircle } from 'lucide-react'
 
 import './styles.css'
 
@@ -12,6 +14,8 @@ export default function HomePage() {
         <h1>Test</h1>
         <b1>Test</b1>
         <b4>Test</b4>
+      </div>
+      <div className="flex items-center gap-3">
         <Button label="Button" color="natural" size="sm" variant="pill" />
         <Button label="Button" color="primary" size="md" variant="pill" />
         <Button label="Button" color="secondary" size="lg" variant="pill" />
@@ -22,6 +26,14 @@ export default function HomePage() {
         <Button label="Button" color="secondary" size="md" variant="rounded" />
         <Button label="Button" color="primary" size="lg" variant="rounded" />
         <Button label="Button" color="natural" size="xl" variant="rounded" />
+      </div>
+      <div className="flex items-center gap-3 justify-center flex-wrap ">
+        <Badge icon={<CodeIcon/>} text="React / Next.js" />
+        <Badge icon={<Database/>} text="PostgreSQL" />
+        <Badge icon={<Cpu/>} text="Python AI" />
+        <Badge icon={<Globe/>} text="Cloud Native" />
+        <Badge icon={<CheckCircle/>} text="Tailwind CSS" />
+        <Badge icon={<CodeIcon/>} text="TypeScript" />
       </div>
     </div>
   )
