493098e70e9998d072f624a36896c344fea41dad
Thaniwat <655021000352@mail.rmutk.ac.th.com>
2025-12-24 16:35:56 +0700

    create badge

---
+++ src/components/commons/badge.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface BadgeProps {
+  icon: React.ReactNode;
+  text: string;
+  className?: string;
+}
+
+export const Badge: React.FC<BadgeProps> = ({ icon, text, className = '' }) => {
+  return (
+    <div 
+      className={`
+        inline-flex items-center gap-2 px-4 py-2 
+        rounded-full shadow-sm 
+        text-gray-700 font-medium text-sm
+        ${className} 
+      `}
+    >
+      <span className="text-natural flex items-center justify-center [&>svg]:w-4 [&>svg]:h-4">
+        {icon}
+      </span>
+      
+      <span>{text}</span>
+    </div>
+  );
+};;
