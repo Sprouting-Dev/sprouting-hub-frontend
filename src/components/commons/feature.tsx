6338d2000cd3fc7f43dd2e0e3e6394b21845d701
Thaniwat <655021000352@mail.rmutk.ac.th.com>
2026-01-06 13:58:12 +0700

    create feature

---
+++ src/components/commons/feature.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { cn } from './util/cn';
+
+interface FeatureProps {
+  icon: React.ReactNode;
+  title: string;
+  detail: string;
+  className?: string;
+}
+
+export const Feature: React.FC<FeatureProps> = ({ icon, title, detail, className }) => {
+  return (
+    <div 
+      className={cn('flex flex-col items-center text-center py-5 w-auto rounded-2xl shadow-md',
+        className
+      )}
+    >
+      <div className="bg-[#E0F2F1] text-primary p-4 rounded-xl mb-3 [&>svg]:w-8 [&>svg]:h-8">
+        {icon}
+      </div>
+      
+      <h6 className="text-natural mb-3">
+        {title}
+      </h6>
+      
+      <b2 className="text-gray-700">
+        {detail}
+      </b2>
+    </div>
+  );
+};
