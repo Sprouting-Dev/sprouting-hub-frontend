f08cf14203bdc5bb5d2b0fef7ae2b61559756385
Kiattikoon-exe <kknippy@gmail.com>
2026-01-16 13:18:12 +0700

    fix: horizon card color style.css onclick prop และเพิ่มสีที่ไม่มีใน style

--- src/components/cards/horizonCard.tsx
+++ src/components/cards/horizonCard.tsx
@@ -8,19 +8,20 @@ interface HorizonCardProps {
   detail: string
   tag: string
   buttonTitle: string
+  onClick?: () => void
   className?: string
 }
 
-const HorizonCard = ({ image, title, detail, tag, buttonTitle, className }: HorizonCardProps) => {
+const HorizonCard = ({ image, title, detail, tag, buttonTitle, onClick, className }: HorizonCardProps) => {
   return (
     <div
       className={cn(
-        'sprout-horizon-card flex flex-col md:flex-row overflow-hidden bg-dark hover:bg-primary transition-colors duration-300',
+        'sprout-horizon-card flex flex-col md:flex-row overflow-hidden',
         className,
       )}
     >
       {/* image - Left Side */}
-      <div className="relative flex-shrink-0 w-full md:w-2/5 aspect-video md:aspect-auto md:self-stretch bg-dark flex items-center justify-center overflow-hidden">
+      <div className="relative flex-shrink-0 w-full md:w-2/5 aspect-video md:aspect-auto md:self-stretch flex items-center justify-center overflow-hidden">
         <Image
           src={image}
           alt={title}
@@ -30,14 +31,17 @@ const HorizonCard = ({ image, title, detail, tag, buttonTitle, className }: Hori
         />
       </div>
       {/* Content - Right Side */}
-      <div className="flex-1 flex flex-col justify-center gap-2 p-8 bg-spt-neutral-900">
+      <div className="flex-1 flex flex-col justify-center gap-2 p-8 border-t-1 border-spt-neutral-850">
         <a href="#" className="text-spt-secondary-400 text-sm font-bold uppercase tracking-wider underline">
           {tag}
         </a>
         <h3 className="text-surface text-2xl font-bold">{title}</h3>
-        <p className="text-muted text-base text-surface leading-relaxed pr-8">{detail}</p>
+        <p className="text-spt-neutral-150 text-base leading-relaxed pr-20">{detail}</p>
         <div className="mt-5">
-          <button className="flex items-center justify-center gap-3 px-6 mt-4 py-2 border-1 rounded-full bg-transparent transition-all duration-300 hover:scale-105 active:scale-95 font-medium border-primary text-primary hover:bg-primary hover:text-white">
+          <button
+            onClick={onClick}
+            className="flex items-center justify-center gap-3 px-6 mt-4 py-2 border-2 rounded-full bg-transparent transition-all duration-300 hover:scale-105 active:scale-95 font-medium border-spt-secondary-400 text-spt-secondary-400 hover:bg-spt-secondary-400 hover:text-white"
+          >
             {buttonTitle}
             <SquareArrowOutUpRight size={20} />
           </button>
