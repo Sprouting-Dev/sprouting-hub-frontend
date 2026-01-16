81d35a6cafaf1c7cb1467a5a611cf573a016fee1
ploywutt <ploy.wutt@gmail.com>
2026-01-16 13:52:34 +0700

    fix(test): add i18n test and remove playwrite

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -6,3 +6,7 @@ import 'dotenv/config'
 if (!process.env.PAYLOAD_SECRET) {
   process.env.PAYLOAD_SECRET = 'test-secret'
 }
+
+if (!process.env.DATABASE_URI) {
+  process.env.DATABASE_URI = 'file:./test.db'
+}
