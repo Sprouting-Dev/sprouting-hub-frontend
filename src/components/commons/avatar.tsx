885d764507eade2aaf9debb375a54ea4ac3d4516
Thaniwat <655021000352@mail.rmutk.ac.th.com>
2026-02-02 15:18:03 +0700

    fix: avatar key and add user icon

--- src/components/commons/avatar.tsx
+++ src/components/commons/avatar.tsx
@@ -2,26 +2,31 @@
 
 import React from 'react'
 import Image, { StaticImageData } from 'next/image'
-import { cn } from '@/util/cn' // นำ cn มาใช้เผื่อ override style
+import { User } from 'lucide-react'
+import { cn } from '@/util/cn'
 
 interface AvatarProps {
   name: string
   role: string
-  src: string | StaticImageData
+  src?: string | StaticImageData
   className?: string
 }
 
 export const Avatar: React.FC<AvatarProps> = ({ name, role, src, className }) => {
   return (
     <div className={cn('flex flex-col items-center text-center gap-4', className)}>
-      <div className="relative w-50 h-50 rounded-full border-4 border-white shadow-xl overflow-hidden bg-black/20">
-        <Image
-          src={src}
-          alt={name}
-          fill
-          className="object-cover"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+      <div className="flex items-center justify-center relative w-50 h-50 rounded-full border-4 border-white shadow-xl overflow-hidden bg-black/20">
+        {src ? (
+          <Image
+            src={src}
+            alt={name}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        ) : (
+          <User className="w-30 h-30 text-gray-400" />
+        )}
       </div>
       <div className="flex flex-col gap-1">
         <h6 className="text-xl text-spt-neutral-1000">{name}</h6>
